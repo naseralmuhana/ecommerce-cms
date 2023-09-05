7cc91dfda06ad27499fcb44bdd528de814cec3ed
naseralmuhana <naseralmuhana0@gmail.com>
2023-09-05 12:14:01 +0300

    changes

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,8 +1,4 @@
-import type { Store } from "@prisma/client"
-
 import prismadb from "@/lib/prismadb"
-
-export const dynamic = "force-static"
 
 interface DashboardPageProps {
   params: { storeId: string }
@@ -13,13 +9,4 @@ export default async function DashboardPage({ params }: DashboardPageProps) {
     where: { id: params.storeId },
   })
   return <div>DashboardPage {store?.name}</div>
-}
-
-export async function generateStaticParams({ params }: DashboardPageProps) {
-  const stores = await prismadb.store.findMany({
-    where: { id: params.storeId },
-  })
-  return stores.map((store: Store) => ({
-    storeId: store.id,
-  }))
 }
